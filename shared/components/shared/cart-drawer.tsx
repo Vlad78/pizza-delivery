45055acf285efecaca08cde83c269ec4ae3eb263
Vlad Razvenkov <dssdom@gmail.com>
2024-08-07 20:24:19 +0200

    feat: empty cart styles

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import { ArrowRight } from 'lucide-react'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { PropsWithChildren, useEffect } from 'react'
 
-import { Button, Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/shared/components/ui'
+import { CartItemWithNestedFields } from '@/@types/prisma'
+import {
+    Button, Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger
+} from '@/shared/components/ui'
 import { calcTotalProductPrice, getStringOfIngredients } from '@/shared/lib/'
 import { cn } from '@/shared/lib/utils'
 import { useCart } from '@/shared/store/cart'
 
-import { CartItemWithNestedFields } from '../../../@types/prisma'
 import { CartDrawerItem } from './cart-drawer-item'
+import { Title } from './title'
 
 
 interface Props {
@@ -86,43 +90,70 @@ export const CartDrawer = ({
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className={cn(className, 'flex flex-col  pb-0 bg-gray-50')}>
-        <SheetHeader>
-          <SheetTitle>
-            <span className='font-bold'>
-              {items.reduce((acc, item) => acc + item.quantity, 0)} items
-            </span>{' '}
-            in Cart
-          </SheetTitle>
-          {/* <SheetClose /> */}
-        </SheetHeader>
+        {items.length > 0 && (
+          <SheetHeader>
+            <SheetTitle>
+              <span className='font-bold'>
+                {items.reduce((acc, item) => acc + item.quantity, 0)} items
+              </span>{' '}
+              in Cart
+            </SheetTitle>
+          </SheetHeader>
+        )}
+
+        {items.length === 0 && (
+          <div className='flex flex-col items-center justify-center w-72 h-full mx-auto'>
+            <Image
+              src={'/assets/other/empty-box.svg'}
+              alt='empty cart'
+              width={200}
+              height={200}
+            />
+            <Title
+              text='Your cart is empty'
+              size='m'
+              className='font-bold my-2'
+            />
+            <p className='text-center text-neutral-500 mb-5'>
+              Add at least one item to make an order
+            </p>
+            <SheetClose>
+              <Button className='w-56 h-12 text-base' size={'lg'}>
+                <ArrowLeft className='w-5 mr-2' />
+              </Button>
+            </SheetClose>
+          </div>
+        )}
 
         <div className='-mx-6 mt-5 overflow-auto scrollbar flex-1'>
           {items.map(item => CartDrawerItemCreator(item))}
         </div>
 
-        <SheetFooter className='-mx-6 bg-white p-8 mt-auto'>
-          <div className='w-full'>
-            <div className='flex mb-4'>
-              <span className='flex flex-1 text-lg text-neutral-500'>
-                Total
-                <div className='flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2' />
-              </span>
+        {cart.totalPrice > 0 && (
+          <SheetFooter className='-mx-6 bg-white p-8 mt-auto'>
+            <div className='w-full'>
+              <div className='flex mb-4'>
+                <span className='flex flex-1 text-lg text-neutral-500'>
+                  Total
+                  <div className='flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2' />
+                </span>
 
-              <span className='font-bold text-lg'>{cart.totalPrice} zł</span>
+                <span className='font-bold text-lg'>{cart.totalPrice} zł</span>
+              </div>
+              <Link href='/checkout'>
+                <Button
+                  onClick={() => {}}
+                  disabled={false}
+                  type='submit'
+                  className='w-full h-12 text-base'
+                >
+                  Create Order
+                  <ArrowRight className='w-5 ml-2' />
+                </Button>
+              </Link>
             </div>
-            <Link href='/checkout'>
-              <Button
-                onClick={() => {}}
-                disabled={false}
-                type='submit'
-                className='w-full h-12 text-base'
-              >
-                Create Order
-                <ArrowRight className='w-5 ml-2' />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+          </SheetFooter>
+        )}
       </SheetContent>
     </Sheet>
   )
